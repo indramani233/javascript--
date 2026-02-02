eb6d8ddec13486e490543fb326bc957da8d6a962
Kumar Indramani Singh <kumaris@sparkcognition.com>
2026-02-02 10:10:14 +0000

    operations to convert the variables

---
+++ 01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com";
+var accountPassword = "12345";
+accountCity = "Jaipur";
+let accountState;
+
+// accountId=2;
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState]);
+
